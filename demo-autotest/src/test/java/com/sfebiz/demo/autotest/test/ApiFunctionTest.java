470dd9acd9774ba6cc14480e5391ab7c22650d20
guankaiqiang <gkqwt1990@gmail>
2014-09-30 17:04:14 +0800

    update

--- demo-autotest/src/test/java/com/sfebiz/demo/autotest/test/ApiFunctionTest.java
+++ demo-autotest/src/test/java/com/sfebiz/demo/autotest/test/ApiFunctionTest.java
@@ -14,16 +14,17 @@ import java.io.InputStream;
  */
 public class ApiFunctionTest {
     private static final String url = "http://localhost:8080/m.api";
-    private static final String rsa = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDoIjY+VacM/v0q47oQkbE4eVo4AS/Px07EMCmlYmRjY9x1OeippSppQ1eNRIuFCbZRqpMoayDO68UdWPCSqOt1I8Uw03MzVDmy38ZBo6dVTRrqWW9z7vbQQ1nWkEcUWcRTIQIktQ2ptO4AOlZa1x1/zvsNBodTNqhqCGPeTNUwyQIDAQAB";
+    private static final String rsa = "你猜";
     @Test
     public void sayHelloTest() {
         final ApiContext context = new ApiContext("1", 123, rsa);
         Demo_SayHello sayHello = new Demo_SayHello("abc");
         final BaseRequest[] requests = new BaseRequest[]{sayHello};
-        WebRequestUtil.fillResponse(url, context.getParameterString(requests), String.valueOf(System.currentTimeMillis()), true, new ResponseFiller() {
-            public ServerResponse fill(InputStream is) {
-                return context.fillResponse(requests, is);
-            }
-        });
+        WebRequestUtil.fillResponse(url, context.getParameterString(requests), String.valueOf(System.currentTimeMillis()), true,
+                                    new ResponseFiller() {
+                                        public ServerResponse fill(InputStream is) {
+                                            return context.fillResponse(requests, is);
+                                        }
+                                    });
     }
 }
