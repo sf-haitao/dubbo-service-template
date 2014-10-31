ef2e7ca768437342cd5d85293a205d52f743fe92
rendong <rendong6@sf-express.com>
2014-10-31 19:39:50 +0800

    update signature function

--- autotest/src/main/java/com/sfebiz/demo/client/ApiContext.java
+++ autotest/src/main/java/com/sfebiz/demo/client/ApiContext.java
@@ -9,87 +9,45 @@ import com.sfebiz.demo.client.util.Base64Util;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.SocketTimeoutException;
 import java.net.URLEncoder;
-import java.security.KeyStore;
 import java.security.MessageDigest;
-import java.security.PrivateKey;
-import java.security.Signature;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ApiContext {
-    private static final Logger logger     = LoggerFactory.getLogger(ApiContext.class);
-    private static final Object signLocker = new Object();
+    private static final Logger logger       = LoggerFactory.getLogger(ApiContext.class);
+    private static final Object signLocker   = new Object();
+    private static       String rsaPublicKey = null;
 
-    private X509Certificate certificate     = null;
-    private PrivateKey      pk              = null;
-    private String          token           = null;
-    private String          deviceId        = null;
-    private String          appid           = null;
-    private String          location        = null;
-    private long            userId          = 0;
-    private String          deviceInfo      = null;
-    private int             vercode         = 0;
-    private long            tokenExpireTime = 0;
-    private String          deviceToken     = null;
-    private String          phoneNumber     = null;
-    private String          dynamic         = null;
-    private static String rsaPubKey;
+    private String appid           = null;
+    private int    vercode         = 0;
+    private String location        = null;
+    private long   deviceId        = 0;
+    private String deviceSecret    = null;
+    private String deviceToken     = null;
+    private long   userId          = 0;
+    private String userToken       = null;
+    private long   userTokenExpire = 0;
+    private String phoneNumber     = null;
+    private String dynamic         = null;
 
-    public ApiContext(String appid, int vercode, String rsaPublicKey) {
+    public ApiContext(String appid, int vercode) {
         this.appid = appid;
         this.vercode = vercode;
-        rsaPubKey = rsaPublicKey;
     }
 
-    public static void setRsaPubKey(String rsaPublicKey) {
-        rsaPubKey = rsaPublicKey;
+    public ApiContext(String appid, int vercode, String location) {
+        this.appid = appid;
+        this.vercode = vercode;
+        this.location = location;
     }
 
-    /**
-     * 获取用来进行消息加密的rsa公钥
-     *
-     * @return
-     */
-    public static String getRsaPubKey() {
-        if (rsaPubKey != null && !rsaPubKey.isEmpty()) {
-            return rsaPubKey;
-        } else {
-            throw new RuntimeException("rsa public key is miss");
-        }
-    }
-    public void setCertificateWithDeviceToken(String fileName, String dtk) {
-        try {
-            KeyStore ks = KeyStore.getInstance("PKCS12");
-            ks.load(new FileInputStream(fileName), "110110".toCharArray());
-            X509Certificate cert = (X509Certificate)ks.getCertificate(ks.aliases().nextElement());
-            PrivateKey pk = (PrivateKey)ks.getKey(ks.aliases().nextElement(), "110110".toCharArray());
-            setCertificate(cert, pk);
-            deviceToken = dtk;
-        } catch (Exception e) {
-            throw new LocalException(e, "cert error.", LocalException.CERT_BROKEN);
-        }
-    }
-
-    public void setCertificateWithDeviceToken(InputStream fis, String dtk) {
-        try {
-            KeyStore ks = KeyStore.getInstance("PKCS12");
-            ks.load(fis, "110110".toCharArray());
-            X509Certificate cert = (X509Certificate)ks.getCertificate(ks.aliases().nextElement());
-            PrivateKey pk = (PrivateKey)ks.getKey(ks.aliases().nextElement(), "110110".toCharArray());
-            setCertificate(cert, pk);
-            deviceToken = dtk;
-        } catch (Exception e) {
-            throw new LocalException(e, "cert error.", LocalException.CERT_BROKEN);
-        }
+    public static void setRsaPublicKey(String key) {
+        rsaPublicKey = key;
     }
 
     public void setPhoneNumberAndDynamic(String phoneNumber, String dynamic) {
@@ -97,100 +55,47 @@ public class ApiContext {
         this.dynamic = dynamic;
     }
 
-    public void setCertificateWithDeviceToken(byte[] keystore, String dtk) {
-        try {
-            KeyStore ks = KeyStore.getInstance("PKCS12");
-            ks.load(new ByteArrayInputStream(keystore), "110110".toCharArray());
-            X509Certificate cert = (X509Certificate)ks.getCertificate(ks.aliases().nextElement());
-            PrivateKey pk = (PrivateKey)ks.getKey(ks.aliases().nextElement(), "110110".toCharArray());
-            setCertificate(cert, pk);
-            deviceToken = dtk;
-        } catch (Exception e) {
-            throw new LocalException(e, "cert error.", LocalException.CERT_BROKEN);
-        }
+    private synchronized void setDeviceInfo(long deviceId, String deviceSecret, String deviceToken) {
+        this.deviceId = deviceId;
+        this.deviceSecret = deviceSecret;
+        this.deviceToken = deviceToken;
     }
 
-    private synchronized void setCertificate(X509Certificate cert, PrivateKey privateKey) {
-        certificate = cert;
-        pk = privateKey;
-
-        String subject = certificate.getSubjectDN().getName();
-        int cnStart = subject.indexOf("CN=");
-        int cnEnd = subject.indexOf(",", cnStart);
-        String cn = null;
-        if (cnEnd != -1) {
-            cn = subject.substring(cnStart + 3, cnEnd);
-        } else {
-            cn = subject.substring(cnStart + 3);
-        }
-        deviceId = cn;
-        int diStart = subject.indexOf("1.2.4.14.4.8.7.21.2=");
-        int diEnd = subject.indexOf(",", diStart);
-        if (diEnd != -1) {
-            deviceInfo = subject.substring(diStart + 20, diEnd);
-        } else {
-            deviceInfo = subject.substring(diStart + 20);
-        }
-        int appidStart = subject.indexOf("1.2.4.14.4.8.7.21.1=");
-        int appidEnd = subject.indexOf(",", appidStart);
-        //不适用证书中的appid参与业务
-        //        if (appidEnd != -1) {
-        //            appid = subject.substring(appidStart + 20, appidEnd);
-        //        } else {
-        //            appid = subject.substring(appidStart + 20);
-        //        }
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setToken(String tk) {
-        token = tk;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public String getDeviceInfo() {
-        return deviceInfo;
+    public void setUserInfo(long userId, String tk) {
+        this.userId = userId;
+        this.userToken = tk;
     }
 
     public long getUserId() {
         return userId;
     }
 
-    public void setUserId(long id) {
-        userId = id;
+    public String getUserToken() {
+        return userToken;
+    }
+
+    public long getDeviceId() {
+        return deviceId;
     }
 
     public String getAppId() {
         return appid;
     }
 
-    public void setTokenExpireTime(long time) {
-        tokenExpireTime = time;
+    public void setUserTokenExpire(long time) {
+        userTokenExpire = time;
     }
 
-    public long getTokenExpireTime() {
-        return tokenExpireTime;
+    public long getUserTokenExpire() {
+        return userTokenExpire;
     }
 
-    public boolean hasCertificate() {
-        return pk != null;
+    public boolean hasDeviceInfo() {
+        return deviceId != 0 && deviceSecret != null && deviceSecret.length() > 0 && deviceToken != null && deviceToken.length() > 0;
     }
 
     public String getParameterString(BaseRequest<?> request) {
-        return getParameterString(new BaseRequest<?>[]{request});
+        return getParameterString(new BaseRequest<?>[] { request });
     }
 
     public String getParameterString(BaseRequest<?>[] requests) {
@@ -226,8 +131,8 @@ public class ApiContext {
             params.put(CommonParameter.location, location);
         }
 
-        if (deviceId != null && !params.containsKey(CommonParameter.deviceId)) {
-            params.put(CommonParameter.deviceId, deviceId);
+        if (deviceId != 0 && !params.containsKey(CommonParameter.deviceId)) {
+            params.put(CommonParameter.deviceId, "" + deviceId);
         }
 
         if (appid != null && !params.containsKey(CommonParameter.applicationId)) {
@@ -287,7 +192,7 @@ public class ApiContext {
                     } else {
                         errorCode = resp.code;
                     }
-                    //modify by xuedong,guankaiqiang
+
                     if (errorCode != 0) {
                         for (BaseRequest<?> request : requests) {
                             request.systime = resp.systime;
@@ -306,24 +211,7 @@ public class ApiContext {
                 logger.error(e, "parse response error. cid=" + commonResponse.getCid());
             }
         }
-        //serverResponse中将错误码以及错误信息暴露
-        //        //通过LocalException将异常抛出
-        //        if (errorCode != 0) {
-        //            LocalException errorCodeException = new LocalException(errorCode);
-        //            throw errorCodeException;
-        //        }
         return commonResponse;
-    }
-
-    public String getCertEncoded() {
-        if (certificate == null) {
-            throw new RuntimeException("certificate is null.");
-        }
-        try {
-            return Base64Util.encodeToString(certificate.getEncoded());
-        } catch (CertificateEncodingException e) {
-            throw new RuntimeException("certificate error.", e);
-        }
     }
 
     public void fillError(BaseRequest<?> request, int code) {
@@ -338,23 +226,12 @@ public class ApiContext {
     }
 
     private void signRequest(ParameterList params, int securityType) {
-        if (params.containsKey(CommonParameter.signature)) return;
+        if (params.containsKey(CommonParameter.signature)) {
+            return;
+        }
         try {
-            if (pk != null && securityType > 0) {
-                if ("EC".equalsIgnoreCase(pk.getAlgorithm())) {
-                    if (!params.containsKey(CommonParameter.signatureMethod)) {
-                        params.put(CommonParameter.signatureMethod, "ecc");
-                    }
-                } else if ("RSA".equalsIgnoreCase(pk.getAlgorithm())) {
-                    if (!params.containsKey(CommonParameter.signatureMethod)) {
-                        params.put(CommonParameter.signatureMethod, "rsa");
-                    }
-                } else {
-                    if (!params.containsKey(CommonParameter.signatureMethod)) {
-                        params.put(CommonParameter.signatureMethod, "ecc");
-                    }
-                }
-            }
+            params.put(CommonParameter.signatureMethod, "sha1");
+            MessageDigest sha = MessageDigest.getInstance("SHA1");
             StringBuilder sb = new StringBuilder(params.size() * 5);
             List<String> paramNames = new ArrayList<String>(params.keySet());
             Collections.sort(paramNames);
@@ -366,54 +243,34 @@ public class ApiContext {
 
             if (securityType == 0) {
                 sb.append("sfhaitao.xyz!");
-                if (ApiConfig.isDebug) {
-                    logger.info("before sig:" + sb.toString());
-                }
-                MessageDigest sha = MessageDigest.getInstance("SHA1");
-                params.put(CommonParameter.signature,
-                           new String(Base64Util.encode(sha.digest(sb.toString().getBytes("utf-8")), Base64Util.NO_WRAP), "utf-8"));
             } else {
-                if (certificate == null || pk == null) {
-                    throw new RuntimeException("certificate is null.");
+                if (deviceSecret == null || deviceSecret.length() == 0) {
+                    throw new RuntimeException("device secret is null.");
                 }
-                if (ApiConfig.isDebug) {
-                    logger.info("before sig:" + sb.toString());
-                }
-                byte[] bs = sb.toString().getBytes("utf-8");
-                Signature sig = null;
-                if ("EC".equalsIgnoreCase(pk.getAlgorithm())) {
-                    sig = Signature.getInstance("SHA1withECDSA");
-                } else if ("RSA".equalsIgnoreCase(pk.getAlgorithm())) {
-                    sig = Signature.getInstance("SHA1withRSA");
-                } else {
-                    sig = Signature.getInstance("SHA1withECDSA");
-                }
-                byte[] s = null;
-                // 对签名部分进行同步
-                synchronized (signLocker) {
-                    sig.initSign(pk);
-                    sig.update(bs);
-                    s = sig.sign();
-                }
-                String signature = new String(Base64Util.encode(s, Base64Util.NO_WRAP), "utf-8");
-                params.put(CommonParameter.signature, signature);
+                sb.append(deviceSecret);
+
             }
+            if (ApiConfig.isDebug) {
+                logger.info("before sig:" + sb.toString());
+            }
+            params.put(CommonParameter.signature,
+                    new String(Base64Util.encode(sha.digest(sb.toString().getBytes("utf-8")), Base64Util.NO_WRAP), "utf-8"));
         } catch (Exception e) {
             throw new RuntimeException("sign url failed.", e);
         }
     }
 
     private String getParameterStringInternal(ParameterList params, int securityType) {
-        if (token != null) {
+        if (userToken != null) {
             if (!params.containsKey(CommonParameter.token)) {
-                params.put(CommonParameter.token, token);
+                params.put(CommonParameter.token, userToken);
             }
         } else {
             if (((securityType & SecurityType.UserLogin) > 0) | ((securityType & SecurityType.UserTrustedDevice) > 0)) {
                 throw new LocalException(LocalException.TOKEN_MISSING);
             }
             if (securityType > 0) {
-                if (deviceToken != null) {
+                if (deviceSecret != null) {
                     if (!params.containsKey(CommonParameter.deviceToken)) {
                         params.put(CommonParameter.deviceToken, deviceToken);
                     }
