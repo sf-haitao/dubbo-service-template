eaad09e11deafa0e6f2374cf6ba74a5b995b057d
guankaiqiang <gkqwt1990@gmail>
2014-11-04 21:19:31 +0800

    refactor throw ServiceException

--- service/src/main/java/com/sfebiz/demo/common/ThrowableCatcher.java
+++ service/src/main/java/com/sfebiz/demo/common/ThrowableCatcher.java
@@ -3,6 +3,7 @@ package com.sfebiz.demo.common;
 import com.alibaba.fastjson.JSON;
 import com.sfebiz.demo.entity.DemoReturnCode;
 import net.pocrd.dubboext.DubboExtProperty;
+import net.pocrd.entity.ServiceException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,15 +17,19 @@ import org.slf4j.LoggerFactory;
 public class ThrowableCatcher {
     private Logger logger = LoggerFactory.getLogger(ThrowableCatcher.class);
     @Around(value = "within(com.sfebiz.demo.service.http.*)")//此处声明需要作用的http api包名
-    public Object doThrowableCatch(ProceedingJoinPoint pjp) {
+    public Object doThrowableCatch(ProceedingJoinPoint pjp) throws ServiceException {
         Object retVal = null;
         try {
             retVal = pjp.proceed();
+        } catch (ServiceException se) {
+            logger.error("error occured", se);
+            throw se;
         } catch (Throwable throwable) {
             logger.error(
                     "execute failed, message: {\"method\":\"" + pjp.getSignature() + "\",\"arguments\":" + JSON.toJSONString(pjp.getArgs()) + "!",
                     throwable);
-            DubboExtProperty.setErrorCode(DemoReturnCode.DEMO_UNKNOW_ERROR);//这个地方设置内部异常error code
+            //            DubboExtProperty.setErrorCode(DemoReturnCode.DEMO_UNKNOW_ERROR);//这个地方设置内部异常error code
+            throw new ServiceException(DemoReturnCode.DEMO_UNKNOW_ERROR);
         }
         return retVal;
     }
