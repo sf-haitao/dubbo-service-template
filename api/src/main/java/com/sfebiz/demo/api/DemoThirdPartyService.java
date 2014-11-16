fcc07db1dfb5af0799df56e666a58726d6be6b35
rendong <leorendong@hotmail.com>
2014-11-16 19:15:26 +0800

    remove useless code

--- api/src/main/java/com/sfebiz/demo/api/DemoThirdPartyService.java
+++ api/src/main/java/com/sfebiz/demo/api/DemoThirdPartyService.java
@@ -1,8 +1,10 @@
 package com.sfebiz.demo.api;
 
+import net.pocrd.entity.ServiceException;
+
 /**
  * Created by guankaiqiang521 on 2014/10/22.
  */
 public interface DemoThirdPartyService {
-    public int testThirdParty(int id);
+    public int testThirdParty(int id) throws ServiceException;
 }
