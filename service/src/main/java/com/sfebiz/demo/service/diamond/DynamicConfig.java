cc052ee85b2ce637cc31751830e6610ef64646ba
leno.lix <leno.lix@taobao.com>
2014-12-14 16:17:45 +0800

    更新diamond示例

--- service/src/main/java/com/sfebiz/demo/service/diamond/DynamicConfig.java
+++ service/src/main/java/com/sfebiz/demo/service/diamond/DynamicConfig.java
@@ -2,7 +2,13 @@ package com.sfebiz.demo.service.diamond;
 
 import com.taobao.diamond.manager.ManagerListener;
 import com.taobao.diamond.manager.impl.DefaultDiamondManager;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.StringReader;
+import java.util.Properties;
 import java.util.concurrent.Executor;
 
 /**
@@ -11,29 +17,69 @@ import java.util.concurrent.Executor;
  * Since: 14/11/26 下午11:56
  */
 public class DynamicConfig {
-    public static void main(String[] args) {
-        ManagerListener testManagerListener = new TestDiamond();
-        DefaultDiamondManager defaultDiamondManager = new DefaultDiamondManager("com.sfebiz.diamond.test", testManagerListener);
-        String configInfo = defaultDiamondManager.getAvailableConfigureInfomation(1000);
-        parseConfigInfo(configInfo);
 
-    }
-    static class TestDiamond implements ManagerListener {
+    private String xxxConfig;
+    private Integer yyyConfig;
 
-        @Override
+    private final static Logger logger = LoggerFactory.getLogger(DynamicConfig.class);
+
+    private static final DynamicConfig dynamicConfig = new DynamicConfig();
+
+    private ManagerListener dynamicConfigListener = new ManagerListener() {
+        public void receiveConfigInfo(String configInfo) {
+            configInfo = StringUtils.strip(configInfo);
+            if (StringUtils.isNotBlank(configInfo)) {
+                logger.warn("Change alipush-ps config: " + configInfo);
+                Properties prop = new Properties();
+                try {
+                    prop.load(new StringReader(configInfo));
+                    updateConfig(prop);
+                } catch (Exception e) {
+                    logger.error("Parse alipush-ps config failed:" + configInfo, e);
+                }
+            }
+        }
+
         public Executor getExecutor() {
             return null;
         }
+    };
 
-        @Override
-        public void receiveConfigInfo(String configInfo) {
-            parseConfigInfo(configInfo);
-        }
+    private void updateConfig(Properties properties) throws Exception {
+        BeanUtils.copyProperties(this, properties);
     }
-    public static void parseConfigInfo(String configInfo) {
 
-        if (null != configInfo && !(configInfo.trim()).equals("")) {
-            System.out.println(configInfo);
-        }
+    public static DynamicConfig getInstance() {
+        return dynamicConfig;
     }
+
+    private DynamicConfig() {
+        String groupId = "DEFAULT_GROUP";
+        String dataId = "com.sfebiz.diamond.test";
+        DefaultDiamondManager defaultDiamondManager = new DefaultDiamondManager(groupId, dataId, dynamicConfigListener);
+        String configInfo = defaultDiamondManager.getAvailableConfigureInfomation(1000);
+        dynamicConfigListener.receiveConfigInfo(configInfo);
+    }
+
+    public String getXxxConfig() {
+        return xxxConfig;
+    }
+
+    public void setXxxConfig(String xxxConfig) {
+        this.xxxConfig = xxxConfig;
+    }
+
+    public Integer getYyyConfig() {
+        return yyyConfig;
+    }
+
+    public void setYyyConfig(Integer yyyConfig) {
+        this.yyyConfig = yyyConfig;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("xxxConfig: " + DynamicConfig.getInstance().getXxxConfig());
+        System.out.println("yyyConfig: " + DynamicConfig.getInstance().getYyyConfig());
+    }
+
 }
